88fbdfcde529557a4645764629416011c317aecf
Arjun Thapa <bad.boy7.bb26@gmail.com>
2017-12-29 17:33:18 +0545

    Mailey pahilo patak files haru add garey.

---
+++ src/demo/Demo.java
@@ -0,0 +1,50 @@
+package demo;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFrame;
+
+public class Demo extends JFrame implements MouseListener {
+   /* public void windowClosing(WindowEvent e) {  
+                dispose(); 
+    }*/
+    
+    event e = new event();
+    public Demo()
+    {
+        
+        this.addMouseListener(this);
+        setSize(400,400);
+        setVisible(true);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+       
+        setBackground(Color.red);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        setBackground(Color.GRAY);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        setBackground(Color.magenta);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        setBackground(Color.yellow);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        setBackground(Color.blue);
+    }
+    public static void main(String args[]){
+       Demo d = new Demo();
+      d.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
